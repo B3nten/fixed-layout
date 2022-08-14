2e8617262d632c46974a1c330bee7028165d7833
Benton Boychuk-Chorney <ben@benten.ca>
2022-08-13 23:33:08 -0600

    second test

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,17 +10,17 @@ export default function App({ Component, pageProps }: { Component: any; pageProp
 
 function FixedLayout({ children }: { children?: React.ReactNode }) {
 	return (
-		<div style={{ position: 'fixed', top: 0, right: 0, left: 0, bottom: 0 }}>
-			<div
-				style={{
-					width: '100%',
-					height: '150vh',
-					position: 'relative',
-				}}>
-				<div style={{ position: 'sticky', width: '100%', height: '100vh', top: 0 }}>
-					{children}
-				</div>
+		// <div style={{ position: 'fixed', top: 0, right: 0, left: 0, bottom: 0 }}>
+		<div
+			style={{
+				width: '100%',
+				height: '150vh',
+				position: 'relative',
+			}}>
+			<div style={{ position: 'sticky', width: '100%', height: '100vh', top: 0 }}>
+				{children}
 			</div>
 		</div>
+		// </div>
 	)
 }

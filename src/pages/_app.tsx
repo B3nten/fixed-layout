dd80af8e2fd7a4536cf61cb604efd38b1411b4a4
Benton Boychuk-Chorney <ben@benten.ca>
2022-08-14 00:27:00 -0600

    Test with pos fixed

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -11,16 +11,16 @@ export default function App({ Component, pageProps }: { Component: any; pageProp
 function FixedLayout({ children }: { children?: React.ReactNode }) {
 	return (
 		// <div style={{ position: 'fixed', top: 0, right: 0, left: 0, bottom: 0 }}>
-		<div
-			style={{
-				width: '100%',
-				height: '150vh',
-				position: 'relative',
-			}}>
+		// <div
+		// 	style={{
+		// 		width: '100%',
+		// 		height: '150vh',
+		// 		position: 'relative',
+		// 	}}>
 			<div style={{ position: 'sticky', width: '100%', height: '100vh', top: 0, overflow: 'hidden' }}>
 				{children}
-			</div>
-		</div>
+			{/* </div>
+		</div> */}
 		// </div>
 	)
 }

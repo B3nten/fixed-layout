bd35bad034a5d5ee1a033e9bdec64d1675deb36f
Benton Boychuk-Chorney <ben@benten.ca>
2022-08-14 01:00:05 -0600

    test

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useLayoutEffect, useRef } from 'react';
 import '../styles/globals.css'
 
 export default function App({ Component, pageProps }: { Component: any; pageProps: any }) {
@@ -9,8 +10,20 @@ export default function App({ Component, pageProps }: { Component: any; pageProp
 }
 
 function FixedLayout({ children }: { children?: React.ReactNode }) {
+
+  const scrollContainer = useRef<HTMLDivElement>(null);
+
+  useIsomorphicLayoutEffect(()=>{
+    function handler(){
+      window.scrollTo(0,0)
+    }
+    window.addEventListener('scroll', debounce(handler,200))
+    return ()=>window.removeEventListener('scroll', debounce(handler,200))
+  },[])
+
 	return (
 		<div
+      ref={scrollContainer}
 			style={{
 				width: '100%',
 				height: '101vh',
@@ -29,3 +42,22 @@ function FixedLayout({ children }: { children?: React.ReactNode }) {
 		</div>
 	)
 }
+
+function debounce(func:any, wait:any, immediate?: any) {
+  //@ts-ignore
+  var timeout;
+  return function() {
+  	var context = this, args = arguments;
+    //@ts-ignore
+  	clearTimeout(timeout);
+  	timeout = setTimeout(function() {
+  		timeout = null;
+  		if (!immediate) func.apply(context, args);
+  	}, wait);
+  	if (immediate && !timeout) func.apply(context, args);
+  };
+}
+
+const useIsomorphicLayoutEffect =
+
+  typeof window !== 'undefined' ? useLayoutEffect : useEffect

24f3ff613162d86b80288b0d67c35ae69ce736ef
Benton Boychuk-Chorney <ben@benten.ca>
2022-08-13 23:29:18 -0600

    first test

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,26 @@
+import '../styles/globals.css'
 
-export default function App({ Component, pageProps }){
-  return <Component {...pageProps} />;
-};
-
-
-
-function FixedLayout({ children }: {children?: React.ReactNode}) {
-  return (
-    <div>
-      <div>
-         {children}
-      </div>
-    </div>
-  );
+export default function App({ Component, pageProps }: { Component: any; pageProps: any }) {
+	return (
+		<FixedLayout>
+			<Component {...pageProps} />
+		</FixedLayout>
+	)
 }
 
-
+function FixedLayout({ children }: { children?: React.ReactNode }) {
+	return (
+		<div style={{ position: 'fixed', top: 0, right: 0, left: 0, bottom: 0 }}>
+			<div
+				style={{
+					width: '100%',
+					height: '150vh',
+					position: 'relative',
+				}}>
+				<div style={{ position: 'sticky', width: '100%', height: '100vh', top: 0 }}>
+					{children}
+				</div>
+			</div>
+		</div>
+	)
+}
